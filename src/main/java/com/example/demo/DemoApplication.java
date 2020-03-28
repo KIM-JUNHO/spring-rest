bd3705268c9fd31240d5eb76afc9519a989ee793
Unknown <prpr48@naver.com>
2020-03-28 18:11:05 +0900

    Corona

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,20 +2,14 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-@RestController
+@EnableScheduling
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-	}
-
-	@RequestMapping("/")
-	public String hello() {
-		return "hello";
 	}
 
 }
